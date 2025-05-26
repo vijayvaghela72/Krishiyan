1fbff2c89ea83d82b42453b7d183955e1509ccfe
Vijay Vaghela <admin@Vijays-MacBook-Pro.local>
2025-05-26 12:52:16 +0530

    some changes has been done

--- lib/screen/dashboard/enquiry/Enquiry/EnquiryDashboardPage.dart
+++ lib/screen/dashboard/enquiry/Enquiry/EnquiryDashboardPage.dart
@@ -5,12 +5,12 @@ import 'package:flutter/services.dart';
 import 'package:krishiyan/helper/AlertHelper.dart';
 import 'package:krishiyan/helper/api_base_helper.dart';
 import 'package:krishiyan/localization/AppLocalizations.dart';
+import 'package:krishiyan/screen/dashboard/enquiry/widget/EnquiryDetailPage.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../../../../mvc/controller/enquiryDashboardController.dart';
 import '../../../../mvc/model/GetAllEnquiryData.dart';
 import '../../../../mvc/model/SelectCropNamesData.dart';
 import '../../../../widgets/constant.dart';
-import 'EnquiryDetailPage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
